8014821c5ca7e1dd3c1a26c52dc43377d930293f
Sergey Varaksin <varaksin86@gmail.com>
2013-07-29 22:17:44 -0500

    init

---
+++ lib/l2met/syslog.rb
@@ -0,0 +1,7 @@
+require "l2met/syslog/version"
+
+module L2met
+  module Syslog
+    # Your code goes here...
+  end
+end
---
+++ lib/l2met/syslog/version.rb
@@ -0,0 +1,5 @@
+module L2met
+  module Syslog
+    VERSION = "0.0.1"
+  end
+end
